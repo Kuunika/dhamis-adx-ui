ecb460845d78396340a4a1ce9fd3e96c2e9a2a0e
ibwighane <bsmwalwanda@gmail.com>
2020-11-17 08:00:54 +0200

    cleared cy data

--- cypress/integration/loading.js
+++ cypress/integration/loading.js
@@ -1,10 +1,5 @@
 /// <reference types="cypress" />
 
-describe("App suit", () => {
-  it("Visits the Kitchen Sink", async () => {
-    await cy.visit("http://localhost:3001/");
-    await cy.get("#description").type("Description");
-    await cy.get("#migrate").click();
-    await cy.get("#year").type(2000);
-  });
+describe("DHAMIS ui", () => {
+  it("Cypress", async () => {});
 });
